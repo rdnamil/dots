f0a57d8a9101b010c19346226dcade120203499b
andrel <rdnamil@github.com>
2025-12-04 16:17:34 -0400

    Updated dots

--- README.md
+++ README.md
@@ -1,12 +1,84 @@
 # Rdnamil's EndeavourOS dots
-My EndeavourOS dotfiles. If you're looking for my quickshell configs, it's been moved over to [quickshell](https://github.com/rdnamil/quickshell). 
+> **_NOTE:_** If you're looking for my quickshell configs, it's been moved over to [quickshell](https://github.com/rdnamil/quickshell).
 
-## Batteries included
-- fastfetch
-- fuzzel
-- ghostty
-- hyprlock
-- kanata
-- niri
-- swayidle
-- random scripts
+![screenshot](./resources/screenshot_001.png)
+
+> [!WARNING]
+> Use at your own risk. This works for me, it may not work for your system!
+
+## What's included
+---
+#### DE
+ - wm: niri [^1]
+ - greeter: greetd [^1]
+ - term: ghostty [^1]
+ - shell: zsh [^1]
+ - file manager: thunar-devel [^1]
+ - idle manager: swayidle [^1]
+ - lockscreen: hyprlock [^1]
+ - wallpaper daemon: swww
+ - wall manager: waypaper
+ - bar: quickshell [^1]
+ - browser: brave
+ - image viewer: ristretto
+ - system monitor: mission center
+
+#### Utils
+ - bat
+ - tree
+ - eza
+ - zoxide
+ - fastfetch [^1]
+ - cava-git [^1]
+ - timeshift
+ - baobab
+ - virt-manager
+ - piper
+ - openRGB [^1]
+ - nwg-look [^1]
+ - qt5/6ct-kde [^1]
+ - sunsetr [^1]
+ - kanata [^1]
+ - plymouth [^1]
+
+#### Gaming
+##### Utils
+ - GPU drivers
+ - gamescope
+ - gamemode
+ - mangohud
+ - obs-vkcapture
+ - oversteer
+##### Launchers
+ - steam
+ - lutris
+ - herioc-launcher
+ - prism-launcher
+ 
+#### Misc
+ - kate [^1]
+ - okular
+ - libre-office _fresh_
+ - obsidian
+ - inkscape
+ - krita
+ - gimp
+ - qbittorrent
+ - obs-studio
+ - legcord
+
+[^1]: config included
+## Installing
+---
+### Requirements
+EndeavourOS with no DE installed on a _btrfs_ formatted partition and _grub_ bootloader.
+
+### Installing
+1. Clone this repository 
+	`git clone https://github.com/rdnamil/dots`
+2. Run setup.sh
+	`dots/setup.sh [driver]`
+	_driver options can include:
+	--amd
+	--nvidia
+	or --intel
--- setup.sh
+++ setup.sh
@@ -1,32 +1,66 @@
+#!/bin/sh
+
 # move to current directory if not already
 cd "$(dirname "$0")" || exit
 
-# post install
+# post EndeavourOS install
+	#enable bluetooth
 sudo systemctl enable bluetooth.service
-mkdir -p ~/.config
-mv -bf config/* ~/.config/
-mkdir -p ~/.local
-mv -bf local/* ~/.local/
-sudo mkdir -p /usr/local/bin
-sudo mv bin/* /usr/local/bin/
+	# move user folders
+mv -bf config ~/.config
+mv -bf local ~/.local
+mv -bf templates ~/templates
+mv -bf zshrc ~/.zshrc
+mv -bf bin /usr/local/bin
 
 # basic DE stuff
+	# install 'niri' and basic pkgs
 yay -S --needed --noconfirm \
 niri xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring polkit-gnome xwayland-satellite xorg-xhost \
 zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting \
 ttf-jetbrains-mono-nerd libappindicator-gtk3 \
-ghostty kate brave-bin swww waypaper mission-center ristretto libreoffice-fresh obsidian qbittorrent piper obs-studio obs-vkcapture lib32-obs-vkcapture partitionmanager gimp inkscape krita baobab bat cava ddcutil fastfetch haruna kdenlive networkmanager-dmenu-git swayidle hyprlock eza openrgb zoxide wl-clipboard wl-clip-persist inotify-tools playerctl sunsetr-bin oversteer new-lg4ff-dkms-git okular
-mv -bf zshrc ~/.zshrc
+ghostty kate brave-bin swww waypaper mission-center ristretto libreoffice-fresh obsidian qbittorrent piper obs-studio obs-vkcapture lib32-obs-vkcapture partitionmanager gimp inkscape krita baobab bat cava ddcutil fastfetch haruna kdenlive networkmanager-dmenu-git swayidle hyprlock eza openrgb zoxide wl-clipboard wl-clip-persist inotify-tools playerctl sunsetr-bin oversteer new-lg4ff-dkms-git okular legcord
+
+	# clone niri config
+git clone https://github.com/rdnamil/niri ~/.config
+
+	# enable ghostty service
 systemctl --user enable app-com.mitchellh.ghostty.service
 
 # greeter
+	# install 'greetd' and 'tuigreet'
 yay -S --needed --noconfirm greetd greetd-tuigreet
+
+	# move configs
 sudo mv -bf greetd.conf /etc/greetd/config.toml
 sudo mv -bf greetd.pam /etc/pam.d/greetd
+
+	# enable greetd service
 sudo systemctl enable greetd.service
 
 # thunar
-yay -S --needed --noconfirm thunar gvfs gvfs-mtp gvfs-smb sshfs thunar-archive-plugin ark thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler ffmpegthumbnailer libgsf tumbler-extra-thumbnailers raw-thumbnailer webp-pixbuf-loader
+	# remove conflicts
+sudo pacman -R --noconfirm libxfce4util || true
+sudo pacman -R --noconfirm libxfce4ui || true
+sudo pacman -R --noconfirm xfce4-dev-tools || true
+
+	# install deps
+yay -S --needed --noconfirm glib2-devel
+yay -S --needed --noconfirm libxfce4util-devel
+yay -S --needed --noconfirm xfce4-dev-tools-devel
+yay -S --needed --noconfirm libxfce4ui-devel
+
+	#install thunar-devel
+yay -S --needed --noconfirm thunar-devel
+
+	# install plugins
+yay -S --needed --noconfirm gvfs gvfs-mtp gvfs-smb sshfs thunar-archive-plugin ark thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler ffmpegthumbnailer libgsf tumbler-extra-thumbnailers raw-thumbnailer webp-pixbuf-loader
+
+	# shares setup
+		# install deps
+yay -S --needed --noconfirm samba
+
+		# setup usershares
 export USERSHARES_DIR="/var/lib/samba/usershares"
 export USERSHARES_GROUP="sambashare"
 sudo mkdir -p ${USERSHARES_DIR}
@@ -38,31 +72,52 @@ sudo mv -bf smb.conf /etc/samba/smb.conf
 sudo systemctl enable smb.service nmb.service
 
 # kanata
+	# install 'kanata'
 yay -S --needed --noconfirm kanata
+
+	# create and add user to 'input' and 'uinput' groups
 sudo groupadd -r uinput
 sudo usermod -aG input $USER
 sudo usermod -aG uinput $USER
-sudo mv -bf 99-input.rules /etc/udev/rules.d/99-input.rules
+
+# cava
+	# downgrade deps
+sudo downgrade --latest --prefer-cache --ignore never autoconf-archive=1:2021.02.19-4 autoconf=2.71-4
+
+	# install cava
+yay -S --needed --noconfirm cava-git
 
 # quickshell
+	# install 'quickshell' and misc deps
 yay -S --needed --noconfirm quickshell brightnessctl songrec
+
+	# clone qs config
 git clone https://github.com/rdnamil/quickshell.git .config/quickshell
 
 # themeing
-yay -S --needed --noconfirm nwg-look gtk-engine-murrine qt5ct-kde qt6ct-kde frameworkintegration frameworkintegration darkly plymouth plymouth-theme-bgrt-no-watermark
-sudo plymouth-set-default-theme -R bgrt-no-watermark
-sudo sed -i -E "s|^( *GRUB_CMDLINE_LINUX_DEFAULT=)(['\"])(.*)(['\"])|\1\2\3 quiet splash\4|" /etc/default/grub
+	# install themeing tools and deps
+yay -S --needed --noconfirm nwg-look gtk-engine-murrine qt5ct-kde qt6ct-kde frameworkintegration darkly plymouth plymouth-theme-bgrt-no-watermark
+
+	# create themes folder
 mkdir -p ~/.local/share/themes
+
+	# clone catppuccin gtk theme
 git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git catppuccin-theme
 catppuccin-theme/themes/install.sh -l -d ~/.local/share/themes -c dark -t blue --tweaks macchiato
+
+	# install 'adwaita++' and 'papirus' icon themes
 wget -qO- https://raw.githubusercontent.com/Bonandry/adwaita-plus/master/install.sh | sh
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 
-# gaming stuff
-yay -S --needed --noconfirm gamescope gamemode lib32-gamemode mangohud lib32-mangohud game-devices-udev lutris steam steamcmd jre-openjdk prismlauncher heroic-games-launcher-bin
-sudo usermod -aG gamemode $USER
+	# plymouth
+		# set plymouth theme
+sudo plymouth-set-default-theme -R bgrt-no-watermark
 
-# drivers
+		# edit grub kernal parameters to show splash on boot
+sudo sed -i -E "s|^( *GRUB_CMDLINE_LINUX_DEFAULT=)(['\"])(.*)(['\"])|\1\2\3 quiet splash\4|" /etc/default/grub
+
+# gaming
+	# install drivers
 for arg in "$@"; do
 	case "$arg" in
 		--amd)
@@ -75,18 +130,40 @@ for arg in "$@"; do
 			yay -S --needed --noconfirm mesa lib32-mesa vulkan-intel lib32-vulkan-intel
 done
 
+	# install gaming utils
+yay -S --needed --noconfirm gamescope gamemode lib32-gamemode mangohud lib32-mangohud game-devices-udev  jre-openjdk
+
+	# add user to 'gamemode' group
+sudo usermod -aG gamemode $USER
+
+	# install launchers
+yay -S --needed --noconfirm lutris steam steamcmd prismlauncher heroic-games-launcher-bin
+
 # virt-manager
+	#install 'qemu' and deps
 yay -S --needed --noconfirm qemu-desktop libvirt virt-manager swtpm
+
+	# add user to 'libvirt' group
 sudo usermod -aG libvirt $USER
+
+	# enable libvirt daemon service
 sudo systemctl enable libvirtd.service
 
 # timeshift
+	# install 'timeshift' and utils
 yay -S --needed --noconfirm timeshift timeshift-autosnap grub-btrfs
+
+	# create and enable grub-btrfs daemon service
 sudo mv -bf grub-btrfsd /etc/systemd/system/grub-btrfsd.service
 sudo systemctl enable --now grub-btrfsd cronie.service
 
 # cleanup
+	# update grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+	# change directory and remove 'dots' dir
 cd ../
 rm -rf dots
+
+# restart
 reboot
